d80d6343f3fdd49e19c656452e41e2cf36f61d5f
Jessica Adamek <jessica.g.adamek@gmail.com>
2024-06-08 15:27:12 -0300

    :lipstick: style signUp page

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,6 +1,7 @@
 import { Label } from '@radix-ui/react-label'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
@@ -25,8 +26,6 @@ export function SignIn() {
       console.log(data)
       await new Promise((resolve) => setTimeout(resolve, 2000))
 
-      throw new Error()
-
       toast.success('Enviamos um link de autenticação para seu email.', {
         action: {
           label: 'Reenviar',
@@ -42,6 +41,11 @@ export function SignIn() {
     <>
       <Helmet title="Login" />
       <div className="p-8">
+        <Button variant="ghost" asChild className="absolute right-8 top-8">
+          <Link to="/sign-up" className="">
+            Novo estabelecimento
+          </Link>
+        </Button>
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
